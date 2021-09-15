cb5412bf64445e9cf3ca9dedfe14803d130dcf32
dlrghks2090 <dlrghks2090@gmail.com>
2021-09-15 18:22:31 +0900

    [baekjoon_정수론 및 조합론_5086번_브론즈2_배수와 약수]

---
+++ 2021.09.15_(01).cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int a, b;
+
+	while (1) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) {
+			break;
+		}
+		else {
+			if (b / a != 0 && b % a == 0) {
+				cout << "factor" << '\n';
+			}
+			else if (a / b != 0 && a % b == 0) {
+				cout << "multiple" << '\n';
+			}
+			else {
+				cout << "neither" << '\n';
+			}
+		}
+	}
+
+
+	return 0;
+}
