e2281675cdf42b567974f43452085b11f73df0d4
dlrghks2090 <dlrghks2090@gmail.com>
2021-11-20 23:29:40 +0900

    [baekjoon_3046번_브론즈5_R2 문제]

---
+++ 2021.11.20_(01).cpp
@@ -0,0 +1,23 @@
+// 백준_3046번_브론즈5_R2 문제
+
+#include<iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+	int r1, s;
+	int r2 = 0;
+
+	cin >> r1 >> s;
+
+
+	r2 = 2 * s - r1;
+
+	cout << r2;
+
+
+	return 0;
+}
